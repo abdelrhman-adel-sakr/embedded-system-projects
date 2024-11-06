944cf50c8a44148b0efc4d5da19d8d9f194fb467
abdelrhman-adel-sakr <abdelrhmansakr248@gmail.com>
2024-11-06 20:00:02 +0200

    Add files via upload
    This repository contains the code and documentation for the Dual Microcontroller-Based Door Locker Security System. The project is designed as a secure, efficient, and user-friendly door locking system that uses two ATmega32 microcontrollers to manage password authentication, motorized door control, and real-time user interaction.
    
    🚀 Project Overview
    This system utilizes two microcontrollers:
    
    HMI_ECU (Human-Machine Interface) for handling user interactions via an LCD and keypad.
    Control_ECU for processing password storage, door control, and security responses.
    The microcontrollers communicate through UART for seamless data transmission, and the system is enhanced with additional features like an external EEPROM for password storage, a PIR motion sensor for detection, and a buzzer for security alerts.
    
    🔑 Key Features
    Password Authentication: Users can set and verify a 5-digit password stored securely in the external EEPROM.
    Motorized Door Control: An H-bridge motor driver unlocks/locks the door based on password entry and motion detection.
    Security Lock: After three incorrect password attempts, the system locks for one minute and triggers a buzzer alarm.
    Motion Detection: The PIR sensor keeps the door open when it detects movement nearby.
    Easy Interface: LCD and keypad integration allow straightforward navigation for password setup, verification, and access control.
    💻 Technical Details
    System Frequency: 8 MHz
    Microcontrollers: ATmega32 (for both HMI_ECU and Control_ECU)
    Communication Protocol: UART (between HMI_ECU and Control_ECU)
    EEPROM: External EEPROM for password storage (via I2C)
    PWM Motor Control: Motor speed management using Timer0 PWM
    Layered Architecture: Drivers include GPIO, UART, I2C, Timer, Buzzer, and PIR sensor for efficient hardware interfacing
    Lockout Feature: If three consecutive incorrect passwords are entered, the system locks and displays an error message.

---
+++ Control_ECU/Debug/MCAL/PWM.d
@@ -0,0 +1,10 @@
+MCAL/PWM.o MCAL/PWM.o: ../MCAL/PWM.c ../MCAL/PWM.h ../MCAL/std_types.h \
+  ../MCAL/common_macros.h ../MCAL/gpio.h
+
+../MCAL/PWM.h:
+
+../MCAL/std_types.h:
+
+../MCAL/common_macros.h:
+
+../MCAL/gpio.h:
---
+++ HMI_ECU/Debug/HAL/lcd.d
@@ -0,0 +1,13 @@
+HAL/lcd.o HAL/lcd.o: ../HAL/lcd.c ../HAL/../MCAL/common_macros.h \
+  ../HAL/lcd.h ../HAL/../MCAL/std_types.h ../HAL/../MCAL/gpio.h \
+  ../HAL/../MCAL/std_types.h
+
+../HAL/../MCAL/common_macros.h:
+
+../HAL/lcd.h:
+
+../HAL/../MCAL/std_types.h:
+
+../HAL/../MCAL/gpio.h:
+
+../HAL/../MCAL/std_types.h:
